a65f52a3c145d9a9e50374cf71b9f40c9958b97a
HusseinMohamed99 <husseinhtm99@gmail.com>
2023-11-05 22:08:32 +0200

    Add new strings and colors

--- lib/core/utils/app_string.dart
+++ lib/core/utils/app_string.dart
@@ -1,4 +1,7 @@
 class AppString {
   static const String appName = "Bookly";
   static const String splashTitle = "Read Free Books";
+  static const String bestSeller = "Best Seller";
+  static const String freePreview = "Free preview";
+  static const String alsoLike = "You can also like";
 }
--- lib/core/utils/color_manager.dart
+++ lib/core/utils/color_manager.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 class ColorManager {
   static const Color scaffoldBackgroundColor = Color(0xff100B20);
   static const Color greyColor = Color(0xff979797);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color orangeColor = Color(0xFFEF8262);
 }
